cb0ff7cd81ea52446784ed5b603bbc8f61cb5b82
Ionut Colceriu <contact@ghinda.net>
2013-12-02 23:13:35 +0200

    Fix some issues with some of the components being retrieved with POST instead of GET.

--- system/desktop/desktop.js
+++ system/desktop/desktop.js
@@ -1,7 +1,7 @@
   $(document).ready(function() {
-    
+
     jQuery.fn.window = function(options) {
-      var settings = jQuery.extend({ width: 400, height: 400, isRun: false },options);            
+      var settings = jQuery.extend({ width: 400, height: 400, isRun: false },options);
       //var window_id = $(this);
       var task_id = $(this).attr("id")+"-task";
       var task_button = "#taskbar #"+task_id;
@@ -9,69 +9,69 @@
       var self = this;
 
       settings.isRun = $(this).attr("class")=="ui-dialog-content";
-                          
+
       if(settings.isRun) {
         self.dialog("activate");
-        self.parent().parent().show(); // show window if minimized        
-      }         
-      else {                      
-        $(this).dialog({        
+        self.parent().parent().show(); // show window if minimized
+      }
+      else {
+        $(this).dialog({
           minWidth: 400,
           minHeight: 400,
           width: settings.width,
           height: settings.height,
-          close: function() { 
+          close: function() {
             $(task_button).hide("slow", function() {
               $("#taskbar #"+task_id).remove();
-            });            
-            $(this).dialog("destroy");           
+            });
+            $(this).dialog("destroy");
           },
-		      activate: function() {						
+		      activate: function() {
             $(".task").removeClass("task-on");
             $(task_button).addClass("task-on");
 		      }
         });
-        
+
         $(this).show();
         if(settings.url) $(this).load("apps/"+settings.url);
         if(settings.script) $(this).getScript(settings.script);
-		    
-		    if(title.length>20) title=title.substr(0, 17)+"..."; 
-        $("#taskbar").append('<div id="'+task_id+'" class="task">'+title+'</div>');        
-		
-		    self.dialog("activate");                       
-		
+
+		    if(title.length>20) title=title.substr(0, 17)+"...";
+        $("#taskbar").append('<div id="'+task_id+'" class="task">'+title+'</div>');
+
+		    self.dialog("activate");
+
         $(task_button).fadeTo("fast", 0.44);
-                        
+
         $(task_button).hover(function() {
           $(this).fadeTo("fast", 0.77);
-          //$(this).addClass("task-hover");        
+          //$(this).addClass("task-hover");
         },
         function() {
-          $(this).fadeTo("fast", 0.44);                  
+          $(this).fadeTo("fast", 0.44);
           //$(this).removeClass("task-hover");
         });
-        
-        $(task_button).click(function() {        
+
+        $(task_button).click(function() {
           self.dialog("activate");
           self.parent().parent().show();
         });
-                               
-      } 
-           
-		return this;                      
-    };        
-    
+
+      }
+
+		return this;
+    };
+
     // get theme, background-color and wallpaper
-    
-    jQuery.fn.initDesktop = function(options) {  
+
+    jQuery.fn.initDesktop = function(options) {
       $.ajax({
         type: "GET",
         url: "system/settings.xml",
         dataType: "xml",
-        success: function(xml) {        
+        success: function(xml) {
           theme = $(xml).find("theme").text();
-          
+
           var url = "system/themes/" + theme + "/" + theme + ".all.css";
           jQuery(document.createElement('link') ).attr({
                 href: url,
@@ -79,42 +79,42 @@
                 type: 'text/css',
                 title: 'theme',
                 rel: 'stylesheet'
-          }).appendTo('head');          
-          
-          $("body").attr("class", theme);        
-        
+          }).appendTo('head');
+
+          $("body").attr("class", theme);
+
           bgcolor = $(xml).find("bgcolor").text();
           $("body").css("background-color", bgcolor);
-        
+
           wallpaper = $(xml).find("wallpaper").text();
-          wallpaper = "url(" + wallpaper + ")";                  
-          $("body").css("background-image", wallpaper);        
-        }                    
-      });      
+          wallpaper = "url(" + wallpaper + ")";
+          $("body").css("background-image", wallpaper);
+        }
+      });
     }
-    
+
     $().initDesktop();
-            
+
     // setup desktop icons
     function setupIcons() {
-      $("#desktop-icons .icon").fadeTo("fast", 0.7);        
+      $("#desktop-icons .icon").fadeTo("fast", 0.7);
       $("#desktop-icons .icon").hover(function() {
         $(this).addClass("icon-hover");
-        $(this).fadeTo("fast", 0.8);        
+        $(this).fadeTo("fast", 0.8);
       },
       function() {
         $(this).removeClass("icon-hover");
         $(this).fadeTo("fast", 0.7);
       });
     }
-                               
-    $("#desktop-icons").load("system/desktop/icons.html",{}, setupIcons);            
+
+    $("#desktop-icons").load("system/desktop/icons.html", null, setupIcons);
     jQuery.getScript("system/desktop/setup.icons.js");
-    
+
     // setup kicker and kickoff
-    var closer;    
+    var closer;
     $("#startmenu").fadeOut();
-    $("#startmenu").load("apps/kickoff/kickoff.html",{});
+    $("#startmenu").load("apps/kickoff/kickoff.html", null);
     $("#kicker").fadeTo("fast", 0.7);
     $("#kicker").hover(function(){
       $(this).fadeTo("fast", 1);
@@ -125,17 +125,17 @@
       closer=setTimeout('$("#startmenu").fadeOut();', 3000);
     });
     $("#kicker").click(function(){
-      $("#startmenu").fadeIn();    
+      $("#startmenu").fadeIn();
     });
     $("#startmenu").hover(function(){
       if(closer) clearTimeout(closer);
     },function() {
       $("#startmenu").fadeOut();
     });
-        
+
   });
-  
+
   $(window).load(function() {
     $("#loader").fadeOut();
-    //setTimeout('$("#loader").fadeOut();', 2000);  
+    //setTimeout('$("#loader").fadeOut();', 2000);
   });
